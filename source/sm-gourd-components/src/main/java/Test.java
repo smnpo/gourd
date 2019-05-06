248d03e611c221875e56b955e6ce2c4b3b76fd19
xing.chen <xing.chen@zhangmen.com>
2019-05-06 14:08:28 +0800

    0.0.1

---
+++ source/sm-gourd-components/src/main/java/Test.java
@@ -0,0 +1,45 @@
+import groovy.lang.GroovyObject;
+import groovy.util.GroovyScriptEngine;
+import groovy.util.ResourceException;
+import groovy.util.ScriptException;
+
+import java.io.IOException;
+
+/**
+ *
+ *
+ * @ClassName    : 	Test
+ * @Description : 	TODO
+ * @author        :	xing.chen
+ * @date        :	2019/5/6
+ *
+ */
+
+public class Test {
+    public static void main(String[] ages){
+        System.out.println("3333");
+        String[] roots = new String[] { "src/main/java/" };
+        //通过指定的roots来初始化GroovyScriptEngine
+
+        try {
+            GroovyScriptEngine gse = new GroovyScriptEngine(roots);
+            GroovyObject groovyObject = (GroovyObject) gse.loadScriptByName("TestScript.groovy").newInstance();
+            GroovyTest gt  = (GroovyTest) gse.loadScriptByName("TestScript.groovy").newInstance();
+            System.out.println( gt.output("5555"));
+            String result = (String) groovyObject.invokeMethod("output", "hello");
+            System.out.println(result);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ResourceException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ScriptException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
